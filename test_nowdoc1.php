9c5ad79361e1e68ceb764c728c11be26a88e6d3a
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2022-12-30 16:31:15 -0300

    Create test_nowdoc1.php
    Exemplo de uso do operador nowdoc

---
+++ test_nowdoc1.php
@@ -0,0 +1,22 @@
+<?php
+   
+   /*A sitaxe nowdoc é basicamente igual a sitaxe heredoc,
+   com excessão de que o identificador de abertura tem que 
+   estar entre aspas simples.
+     O que estiver entre os identificadores de abertura e 
+   fechamento, não serão processados.
+     */
+   
+   echo <<<'INICIO'
+       ESTUDO DE FORMA CONSTANTE
+          PARA QUE O CONHECIMENTO
+            FIQUE SOLIDIFICADO EM
+              MINHA MENTE.
+
+INICIO;
+
+//Não se usa identação antes do identificador de fechamento
+
+
+
+ ?>
