807cc75e9a27bb64e9907234a845078d81279adf
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2022-12-30 16:21:35 -0300

    Create test_heredoc1.php

---
+++ test_heredoc1.php
@@ -0,0 +1,21 @@
+<?php
+   
+   /* A sitaxe heredoc se dá pelo uso do operador(<<<)
+   o identificador de abertura(END), a string e 
+   identificador de fechamento(END o mesmo da abertura)
+   na nova linha sem identação.
+   */
+     
+     //Exemplo 1, heredoc
+
+ echo <<<END
+     MEU ESTUDOS
+       NA LINGUAGUEM PHP
+         ESTÃO PROGREDINDO
+           DE ACORDO COM A MINHA DEDICAÇÃO,
+             PERSEVERANÇA E PACIÊNCIA.
+END;
+
+//Sem identação antes do identificador de fechamento
+
+ ?>
