53404a631d3b9c4be10eafb9a102788f51e16422
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-01 01:07:44 -0300

    Create aspas_duplas.php
    Exemplo 1 de String com aspas duplas

---
+++ aspas_duplas.php
@@ -0,0 +1,22 @@
+<?php
+
+    /* (\", \\, \$) O que estiver entre aspas duplas
+       será interpretado pelo php.
+      */
+
+    $empresa = "\\Light \\";
+    echo "$empresa <br>";
+    
+    
+    $estado_suldeste = "\" Rio de Janeiro \"";
+    echo "$estado_suldeste <br>";
+    
+    
+    $moeda_brasileira = "\$ Real \$ ";
+    echo "$moeda_brasileira <br>";
+    
+    
+    
+   
+
+ ?>
