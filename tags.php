312d9e118866aec367011aa982905c696a697644
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-02 18:03:36 -0300

    Create tags.php
    Estudos de tags do php

---
+++ tags.php
@@ -0,0 +1,21 @@
+<?php 
+ 
+ /*
+ Tudo o que estiver entre as tags(<?php....?>)
+  será interpretado pelo php.
+ */
+ 
+ $estudo = 'Constante'; //Comentário de linha unica usa-se barra dupla ou asterisco
+ $dedicacao = "Constante"; #Exemplo de comentario com asterisco.
+ 
+ 
+  if($estudo == 'Constante' && $dedicacao == 'Constante')
+  {
+    echo 'Permaneça assim, voce terá sucesso em seu propósito';
+  }
+  else
+  {
+    echo 'Voce precisa se esforcar mais para alcancar seu objetivo';
+  }
+ 
+?>
