0e221bb339985eba3e9c75c64e7c267ab847ba2b
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-02 18:54:21 -0300

    Create escape_html.php
    Exemplo de escape de html com php

---
+++ escape_html.php
@@ -0,0 +1,9 @@
+<?php for($i = 0;$i <= 5;$i++): ?>
+ <h1> Exemplo de escape de html com php </h1>
+ 
+ <p>
+     E preciso disciplina para ser um bom desenvolvedor.
+     
+ </p>
+ 
+ <?php endfor; ?>
