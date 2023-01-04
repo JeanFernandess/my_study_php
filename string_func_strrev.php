a8c239e2f364f3285d564fc912a265471dd9c213
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-03 23:16:35 -0300

    Create string_func_strrev.php
    Estudando a função string strrev()

---
+++ string_func_strrev.php
@@ -0,0 +1,15 @@
+<?php
+   $carro_marca = array('Mitsubishi', 'Volkswagen', 'Peugeot', 'Troller');
+   
+   for($i = 0; $i < 3; $i++)
+   {
+   
+     foreach($carro_marca as $marca)
+     {
+       //A função strrev() reverte o conteudo das posições no array $carro_marca.
+       echo "Reverso =>" . strrev($marca) . "<br>";
+       
+     }
+    echo "<br>";
+    
+   }
