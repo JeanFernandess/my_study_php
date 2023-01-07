57356029d497a7e0541a790ffdcecfbce0cbbd4b
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-07 16:52:28 -0300

    Create func_array_shift.php
    Estudo da função array_shif()

---
+++ func_array_shift.php
@@ -0,0 +1,11 @@
+<?php
+   
+   $stack = array('Front-end', 'Back-end','Full-stack', 'Dev-ops');
+   
+   //Essa função remove um item no inicio do array
+   array_shift($stack);
+   array_shift($stack);
+   
+   var_dump($stack);
+
+ ?>
