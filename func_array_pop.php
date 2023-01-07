84741deef478ef8e74db27f3239d79a029ef1f29
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-07 16:47:45 -0300

    Create func_array_pop.php
    Estudo da função array_pop()

---
+++ func_array_pop.php
@@ -0,0 +1,11 @@
+<?php
+   
+   $cores = array('Amarelo', 'Azul','Verde', 'Preto');
+   
+   //Essa função remove um item no final do array $cores
+   array_pop($cores);
+   array_pop($cores);
+   
+   var_dump($cores);
+
+ ?>
