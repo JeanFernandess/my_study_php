d119b3658ec563c948b589df34192eddcd458f4c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-07 16:38:52 -0300

    Create func_array_push.php
    Estudo da função array_push()

---
+++ func_array_push.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $animais = array('Gato', 'Cachorro','Mico', 'Onça');
+   
+   //Essa função adiciona um intem no final do array
+   array_push($animais, 'Capivara');
+   var_dump($animais);
+
+ ?>
