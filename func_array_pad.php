23c46a555866928107bd6152d3b5f2dd30f80110
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-10 16:40:22 -0300

    Create func_array_pad.php
    Estudo da função array_pad() no php

---
+++ func_array_pad.php
@@ -0,0 +1,15 @@
+<?php
+   /*
+     A função array_pad() preenche um array 
+     com um determinado valor de posicões.
+     
+     array_pad(Array,Numero de posicoes, valor das posicoes)
+   */
+       
+     $carros_cor = array('Amarelo', 'Vermelho', 'Azul');
+     
+     $carros_cor = array_pad($carros_cor, 5, 'Branco');
+     
+     var_dump($carros_cor);
+
+ ?>
