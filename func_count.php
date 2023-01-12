9fffe9fd7a6e3cbd53eaf2b8d095caaf19434b73
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-12 17:22:54 -0300

    Create func_count.php
    Estudo da função count() no php

---
+++ func_count.php
@@ -0,0 +1,27 @@
+<?php
+   /*
+     A função count() recebe um array
+     e retorna a quantidade de elementos
+     desse array.
+     
+     count(Array)
+     
+   */
+       
+     
+     
+     $amigos = array('Pessoa_0' => 'Carlos',
+                     'Pessoa_1' => 'Fabiana',
+                     'Pessoa_2' => 'Luana',
+                     'Pessoa_3' => 'Narcelio',
+                     'Pessoa_4' => 'Clara',
+                     'Pessoa_5' => 'Matheus');
+     
+     //count() so retorna a quantidade de elementos
+     
+     
+     echo 'Eu possuo '.count($amigos).' amigos';
+     
+     
+
+ ?>
