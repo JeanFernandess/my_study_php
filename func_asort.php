13a8cff99e788e234b9baa48fe54d905a8e82b29
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-13 23:49:07 -0300

    Create func_asort.php
    Estudo da função asort() no php

---
+++ func_asort.php
@@ -0,0 +1,22 @@
+<?php
+   
+   /*A função asort() ordena o array pelo conteudo
+     mantendo a associação entre os indices.
+
+   */
+   
+        $time_futebol[0] = 'Flamengo';
+        $time_futebol[1] = 'Cruzeiro';
+        $time_futebol[2] = 'Palmeiras';
+        $time_futebol[3] = 'Fluminense';
+        $time_futebol[4] = 'Goiais';
+        $time_futebol[5] = 'Botafogo';
+        $time_futebol[6] = 'Atletico-Mg';
+        $time_futebol[7] = 'Fortaleza';
+        $time_futebol[8] = 'Campinense';
+                   
+   asort($time_fotebol);
+   
+   print_r($time_fotebol);
+   
+?> 
