574982feb13ddf02df39f1014a2ff783c7916dfc
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-14 22:29:25 -0300

    Create func_implode.php
    Estudo da função implode() no php

---
+++ func_implode.php
@@ -0,0 +1,22 @@
+<?php
+
+     /* A função implode converte um array em uma string, usando um 
+       separador para cada elemento.
+     */
+      
+     $array_1 = ["234", "405", "867", "-66"];
+     
+     var_dump(implode(".", $array_1));
+     echo "<br />";
+     
+     $array_2 = ["23", "04", "2047"];
+     
+     var_dump(implode("/", $array_2));
+     echo "<br />";
+     
+     $array_3 = ["3 ", " (4 + 7)"];
+     
+     var_dump(implode("*", $array_3));
+     
+     
+?> 
