bd7c7d9b3d8f0f4ac7069510a3d3405474c105fd
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-17 22:52:49 -0300

    Create do_while.php
    Estudo do loop do_while no php

---
+++ do_while.php
@@ -0,0 +1,15 @@
+<?php 
+       
+       /* O loop do while executa o bloco de codigo pelo menos
+          uma vez e em seguida verifica se a condição e verdadeira,
+          se sim, o bloco de codigo e executado novamente.
+       */
+       
+$x = 10;
+
+do {
+  echo "O numero que esta em X é: $x <br>";
+  $x++;
+} while ($x <= 5);
+
+?>
