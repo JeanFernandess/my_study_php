b76662ba664144ea8568c0f6133fe4e1b5250097
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-17 22:46:33 -0300

    Create loop_while.php
    Estudo do loop while no php

---
+++ loop_while.php
@@ -0,0 +1,14 @@
+<?php  
+      /* O loop while, a cada interação se verdadeiro, 
+        executa o que esta no bloco e em seguida incrementa
+        o contador.
+      */
+      
+$x = 1;
+ 
+while($x <= 5) {
+  echo "O numero dessa interação é: $x <br>";
+  $x++;
+}
+
+?> 
