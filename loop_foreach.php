5af0015697cf2cf382993d4d176ea840c252936d
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-18 23:25:03 -0300

    Create loop_foreach.php
    Estudo do loop Foreach no php

---
+++ loop_foreach.php
@@ -0,0 +1,13 @@
+<?php
+   /* O FOREACH e um laço de repetição para
+      interações com arrays.O vetor e decomposto
+      usando a clausula AS.
+     */
+       
+   $moto = array('honda', 'yamaha', 'suzuky', 'bmw');
+   
+   foreach($moto as $marca){
+     print "$marca é a melhor do mundo!<br/> ";
+   }
+
+ ?>
