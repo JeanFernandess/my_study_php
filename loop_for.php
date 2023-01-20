f104f9f12740f95e5a98d5828652e48383fd9b05
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-19 23:50:29 -0300

    Create loop_for.php
    Estudo do loop for no php

---
+++ loop_for.php
@@ -0,0 +1,11 @@
+<?php  
+
+     /* O loop for percorre um bloco de codigo, um numero 
+        especifico de vezes.Usa-se o loop for quando se 
+        sabe de antimão o numero de repetições.
+        
+     */
+for ($x = 0; $x <= 100; $x+=10) {
+  echo "Um programador deve estudar $x horas de estudo por dia! <br>";
+}
+?>  
