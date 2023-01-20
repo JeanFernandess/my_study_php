0c40b6dea8047ddc6cad1d38a1a64bd8ba6b3387
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-20 00:00:38 -0300

    Create loop_for2.php
    Estudo do loop for no php

---
+++ loop_for2.php
@@ -0,0 +1,18 @@
+<?php  
+
+     
+for ($x = 0; $x <= 100; $x+=10) {
+
+  echo "Um programador deve estudar $x horas de estudo por dia! <br>";
+  
+  if($x == 10){
+     
+    echo 'ATENÇÃO, a partir dessa carga horaria, torna-se IMPOSSIVEL de absorver a informação! <br />';
+    
+  
+  }
+  
+}
+
+
+?>  
