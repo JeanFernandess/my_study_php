c630d6a9e7e44698b477148957bbc91dae54fff2
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-24 00:28:26 -0300

    Create math_fun_sqrt.php
    Estudo basico da função sqrt() no php

---
+++ math_fun_sqrt.php
@@ -0,0 +1,13 @@
+<?php
+      /* A função sqrt() retorna a raiz quadrada do numero passado no argumento
+         da função.
+      */ 
+      
+      
+echo(sqrt(64) . "<br>");
+echo(sqrt(0) . "<br>");
+echo(sqrt(100) . "<br>");
+echo(sqrt(9));
+
+
+?>
