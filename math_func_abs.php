2f6ee52354b1c1d39219ff801b3097f4e1e1e955
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-24 00:24:52 -0300

    Create math_func_abs.php
    Estudo basico da função ABS() no php

---
+++ math_func_abs.php
@@ -0,0 +1,12 @@
+<?php
+     // A função abs() retorna o valor positivo absoluto de um numero.
+     
+     echo(abs(-7.6). "<br>");
+     echo(abs(-23.9). "<br>");
+     echo(abs(-5.9));
+     
+     
+      
+     
+
+?>
