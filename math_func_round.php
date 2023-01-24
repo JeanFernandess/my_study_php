98aadef892feb05a2a9c147106769076df2e9724
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-24 00:34:38 -0300

    Create math_func_round.php
    Estudo basico da função round no php.

---
+++ math_func_round.php
@@ -0,0 +1,16 @@
+<?php
+      /* A função round() arredonda um numero de ponto
+         flutuante para um inteiro mais próximo.
+      */ 
+      
+      
+echo(round(60.4) . "<br>");
+echo(round(0.6) . "<br>");
+echo(round(10.7) . "<br>");
+echo(round(9.3) . "<br>");
+echo(round(100.8) . "<br>");
+echo(round(1.4) . "<br>");
+echo(round(1.8));
+
+
+?>
