1c40c71414bcd8cb1ea76e0085fe5b31a02d289f
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-24 00:14:59 -0300

    Create math_func_min_max.php
    Estudo basico das funções MIN() e MAX() no php

---
+++ math_func_min_max.php
@@ -0,0 +1,11 @@
+<?php
+     /* A função MIN procura o menor numero dentro de uma listagem
+        de argumentos.
+        A função MAX procura pelo maior numero dentro de uma serie
+        de argumentos numéricos.
+     
+     */
+echo(min(0, 15056, 304555, 2044, -8444, -1333) . "<br>");
+echo(max(0, 1.50, 0.30, 2, -8, -200). "<br>");
+
+?>
