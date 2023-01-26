f8aa65ca550de1fa5bf6a082ed8b9318e2cb7e5c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-25 23:58:29 -0300

    Create math_func_rand.php
    Estudo da função rand() no php

---
+++ math_func_rand.php
@@ -0,0 +1,13 @@
+<?php
+     /* A função rand() retorna um numero inteiro aleatório.
+        Usa-se o paramentros pa definir o limite desse inteiro.
+        
+     */
+     
+echo(rand(10, 100). "<br>");
+echo(rand(1, 9). "<br>");
+echo(rand(39, 44). "<br>");
+echo(rand(100, 200). "<br>");
+echo(rand(20, 40));
+
+?>
