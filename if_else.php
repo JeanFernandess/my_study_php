7697c2de661f5225fb1b43823bd68c7b3039ab18
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-26 19:20:49 -0300

    Create if_else.php
    Estudo da estrutura condicional if_else em php

---
+++ if_else.php
@@ -0,0 +1,20 @@
+<?php
+     /* Estrutura condicional IF_ELSE e usada
+        para repetições de um bloco de código. 
+      */
+        
+      $programador = 'Ruim';
+      
+      if($programador == 'Bom'){
+      
+        echo 'Parabens, voce é um bom programador!';
+        
+       }else{
+         
+        echo 'Para ser bom, voçê precisa estudar mais!';
+         
+    }
+       
+       
+       
+ ?>
