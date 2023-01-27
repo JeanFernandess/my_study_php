f33c19c21864daba291323754aaf8a7588669899
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-27 00:01:45 -0300

    Create if_elseif.php
    Estudo da estrutura condicional if_elseif no php

---
+++ if_elseif.php
@@ -0,0 +1,29 @@
+<?php
+     
+        
+      $minhaIdade = 12;
+      
+      if($minhaIdade > 18 && $minhaIdade < 30){
+      
+          echo 'Eu sou adulto';
+        
+              }elseif($minhaIdade < 18 && $minhaIdade > 13){
+         
+                  echo 'Eu sou adolescente';
+         
+                      }elseif($minhaIdade < 13){
+         
+                          echo 'Voce é considerado uma criança';
+                          
+                          }else{
+                            
+                              echo 'Voce é um idoso';
+                              
+                              }
+                              
+                              
+      
+       
+       
+       
+ ?>
