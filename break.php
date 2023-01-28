4828899c16b25791854af3467d4ee57088e4ab87
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-27 23:22:34 -0300

    Create break.php
    Estudo e entendimento da instrução BREAK no php

---
+++ break.php
@@ -0,0 +1,11 @@
+<?php  
+
+     // A instrução BREAK é usada para sair de loops
+     
+for ($x = 0; $x < 10; $x++) {
+  if ($x == 7) {
+    break;
+  }
+  echo "O numero da contagem é: $x <br>";
+}
+?>
