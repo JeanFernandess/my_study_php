cb6ecc434463428569de8895fc119ef986f196de
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-29 22:45:22 -0300

    Create media_aluno.php
    Esse algoritmo soma as notas e retorna a média

---
+++ media_aluno.php
@@ -0,0 +1,21 @@
+<?php
+    
+    $nota_1 = 5.5;
+    $nota_2 = 7.0;
+    $nota_3 = 1.0;
+    $nota_4 = 7.0;
+    
+    $soma = ($nota_1 + $nota_2 + $nota_3 + $nota_4) / 4;
+     
+    if($soma <= 6){
+      
+      echo 'Infelizmente!Voce não passou de ano!<br>';
+      echo "Sua média é: $soma";
+      
+    }else{
+      
+      echo 'Parabens!Voce passou de ano!<br>';
+      echo "Sua média é: $media";
+      }
+
+ ?>
