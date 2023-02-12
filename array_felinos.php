97993758292df2061534a1f3f1ecd9a870ef0631
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-12 20:59:06 -0300

    Create array_felinos.php
    Estudo e pratica de arrays junto com estruturas decisão.

---
+++ array_felinos.php
@@ -0,0 +1,16 @@
+<?php
+
+     //Exemplo simples de um array linear e o foreach fazendo interações com esse array junto com if, elseif, else.
+
+     $felinos = array("Tigre", "Leão", "Onça", "Gato do mato", "Leopardo", "Puma");
+     
+     foreach($felinos as $felino){
+         if($felino == "Leão"){
+             echo " O felino $felino é o MAIS FORTE e PERIGOSO<br>";
+         }elseif($felino == "Gato do mato"){       
+             echo " O felino $felino é o MENOS PERIGOSO!<br>";
+         }else{
+             echo " O felino $felino não deixa de ser PERIGOSO!CUIDADO!<br>";
+         }
+     }
+?> 
