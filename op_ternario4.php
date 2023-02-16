890410a564dd2bc4f7f82ed5bce52a93fb7ca25d
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-16 19:26:18 -0300

    Create op_ternario4.php
    Exemplo simples de uso do operador ternário.

---
+++ op_ternario4.php
@@ -0,0 +1,11 @@
+<?php
+   
+   // Exemplo simples de uso do operador ternario
+
+   $horas_trabalho = 9; 
+   $horas_trabalho = ($horas_trabalho > 8)? 'Voce receberá horas extras!': 'Sua carga horaria e padrao';
+   echo $horas_trabalho;  
+   
+     
+  
+ ?>
