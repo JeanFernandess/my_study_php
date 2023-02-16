49f55c19dafa79669fcfc718463610e57a3765e4
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-15 22:55:01 -0300

    Create constante4_test.php
    Estudo e prática de constantes no php.

---
+++ constante4_test.php
@@ -0,0 +1,19 @@
+<?php
+    
+    // Exemplos simples de uso de constantes
+
+    define("MEU_NOME", "Jean");
+    define("MEU_SOBRENOME", " Fernandes");
+    define("DATA_NASCIMENTO", "12/12/1212");
+    define("VALOR_BITCOIN", 25.000);
+    
+    echo MEU_NOME, MEU_SOBRENOME;
+    echo "<br>";
+    echo 'Minha data de nascimento é: '. DATA_NASCIMENTO;
+    echo "<br>";
+    echo 'O valor atual do bitcoin e : '. VALOR_BITCOIN;
+    
+  
+       
+  
+ ?>
