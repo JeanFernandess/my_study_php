b1775a3ed63a36af0bb6e3e069f327d9977d6400
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-18 00:01:16 -0300

    Update constante4_test.php
    Acréscimo de mais uma constante(VALOR_ETHERIUM).

--- constante4_test.php
+++ constante4_test.php
@@ -6,12 +6,15 @@
     define("MEU_SOBRENOME", " Fernandes");
     define("DATA_NASCIMENTO", "12/12/1212");
     define("VALOR_BITCOIN", 25.000);
+    define("VALOR_ETHERIUM", 1.750);
     
     echo MEU_NOME, MEU_SOBRENOME;
     echo "<br>";
     echo 'Minha data de nascimento é: '. DATA_NASCIMENTO;
     echo "<br>";
     echo 'O valor atual do bitcoin e : '. VALOR_BITCOIN;
+    echo "<br>";
+    echo 'O valor do etherium e : '.VALOR_ETHERIUM;
     
   
        
