addb91e2f3f21b57c4f0219be867770ccb61132a
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-19 23:17:51 -0300

    Update func_array_pop.php
    Foi adicionado um item ao array e mais uma função array_pop.

--- func_array_pop.php
+++ func_array_pop.php
@@ -1,8 +1,9 @@
 <?php
    
-   $cores = array('Amarelo', 'Azul','Verde', 'Preto');
+   $cores = array('Amarelo', 'Azul','Verde', 'Preto', 'Laranja');
    
    //Essa função remove um item no final do array $cores
+   array_pop($cores);
    array_pop($cores);
    array_pop($cores);
    
