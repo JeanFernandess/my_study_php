8035c720d2d8742901a464a89babba19a1e63d8e
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-23 19:10:24 -0300

    Update atribuicao_referencia.php
    Adição das variáveis $numero_3 e $numero_4 e uma divisão a variável $total.

--- atribuicao_referencia.php
+++ atribuicao_referencia.php
@@ -8,9 +8,11 @@
 
     $numero_1 = 1000;
     $numero_2 = &$numero_1; //$numero_2 = 1000
-    
-    $total = $numero_1 + $numero_2;
-    echo $total; //$total = 2000
+    $numero_3 = &$numero_4;
+    $numero_4 = 5;
+
+    $total = ($numero_1 + $numero_2)/$numero_3;
+    echo $total; //$total = 400
     
     
       
