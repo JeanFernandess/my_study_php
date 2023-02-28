d2a5c413089648e333944c76753476c84bbb1435
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-27 22:32:17 -0300

    Update loop_for.php
    Adição das funções echo() dentro da estrutura condicional.

--- loop_for.php
+++ loop_for.php
@@ -1,12 +1,19 @@
-<?php  
+  <?php  
 
      /* O loop for percorre um bloco de codigo, um numero 
-        especifico de vezes.Usa-se o loop for quando se 
-        sabe de antimão o numero de repetições.
+        especifico de vezes.Quando $x for igual 10 o bloco
+        da estrutura condicional e ativado.
+        
         
      */
+       
 for ($x = 0; $x <= 100; $x+=10) {
+  
+  if($x == 10){
   echo "Um programador deve estudar $x horas de estudo por dia! <br>";
-  echo "ATENÇÃO, $x HORAS NÃO E O RECOMENDADO!!";
+  echo "<br>";
+  echo "ATENÇÃO,ACIMA DE $x HORAS NÃO E O RECOMENDADO!!";
+  }
+  
 }
 ?>  
