33a45caca340195c1b22bb230a591beb1846ddeb
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-28 22:07:42 -0300

    Update func_array_pop.php
    Adição Branco e Roxo ao array $cores e uma função array_pop().

--- func_array_pop.php
+++ func_array_pop.php
@@ -1,8 +1,9 @@
 <?php
    
-   $cores = array('Amarelo', 'Azul','Verde', 'Preto', 'Laranja');
+   $cores = array('Amarelo', 'Azul','Verde', 'Preto', 'Laranja', 'Branco', 'Roxo');
    
    //Essa função remove um item no final do array $cores
+   array_pop($cores);
    array_pop($cores);
    array_pop($cores);
    array_pop($cores);
