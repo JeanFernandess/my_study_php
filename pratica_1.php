6d96ebf6c920328f5b792a262b72df464775e6fa
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-02-28 22:04:35 -0300

    Create pratica_1.php
    Fixação dos estudos de array, estrutura condicional e repetição.

---
+++ pratica_1.php
@@ -0,0 +1,29 @@
+<?php  
+ 
+    // Pratica de array, estrutura de repeticao e decisão.
+     
+      
+      
+ 
+    $cryptos = array('Bitcoin', 'Etherium', 'Xrp', 'Xlm', 'Shibainu');
+    
+    foreach($cryptos as $crypto){
+        if($crypto == 'Bitcoin'){
+            echo "A crypto $crypto se provou a melhor ao longo do tempo. <br>";
+      
+            }elseif($crypto == 'Shibainu'){
+                echo "A crypto $crypto e considerada uma moeda meme.<br>";
+      
+                }else{
+                    echo "A crypto $crypto e aceitavel.<br>";
+      
+    }
+    
+ }
+
+    
+  
+
+?>  
+
+      
