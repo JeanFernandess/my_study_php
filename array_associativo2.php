98fb4847ff49b76ae2c2f830c2ab34189aa46d41
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-02 17:31:25 -0300

    Create array_associativo2.php
    Exemplo e prática dos arrays associativos.

---
+++ array_associativo2.php
@@ -0,0 +1,19 @@
+  
+ <?php  
+      
+      
+      // Exemplo se array associativo
+
+   $jean_dados['Nome'] = 'Jean Fernandes Batista';
+   $jean_dados['Idade'] = '28';
+   $jean_dados['Profissão'] = 'Desenvolvedor back-end';
+   $jean_dados['Formação'] = 'Analise de Sistemas';
+   
+   foreach($jean_dados as $key => $dados){
+     
+     echo $key. ' --> ' .$dados. "<br>";
+   }
+    
+  
+
+?>
