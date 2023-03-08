4e9eb8681d52e10e75655030276185d4bec4e9fc
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-07 23:18:58 -0300

    Update math_func_rand.php
    Adição de mais uma função rand().

--- math_func_rand.php
+++ math_func_rand.php
@@ -8,6 +8,7 @@ echo(rand(10, 100). "<br>");
 echo(rand(1, 9). "<br>");
 echo(rand(39, 44). "<br>");
 echo(rand(100, 200). "<br>");
-echo(rand(20, 40));
+echo(rand(20, 40). "<br>");
+echo(rand(40, 150));
 
 ?>
