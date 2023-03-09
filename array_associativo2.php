c0d2a04c3aed7a6ea442b5d0671e31de120e3c89
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-09 17:21:18 -0300

    Update array_associativo2.php
    Adição da Altura ao array $jean_dados().

--- array_associativo2.php
+++ array_associativo2.php
@@ -8,6 +8,7 @@
    $jean_dados['Idade'] = '28';
    $jean_dados['Profissão'] = 'Desenvolvedor back-end';
    $jean_dados['Formação'] = 'Analise de Sistemas';
+   $jean_dados['Altura'] = '1.82';
    
    foreach($jean_dados as $key => $dados){
      
