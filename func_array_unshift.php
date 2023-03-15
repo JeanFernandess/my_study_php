84b79d98b2e1264978cb9188b39fdc9d855790b3
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-14 23:56:28 -0300

    Update func_array_unshift.php
    Adição da marca Troller ao início do array com a função array_unshift().

--- func_array_unshift.php
+++ func_array_unshift.php
@@ -5,6 +5,7 @@
    //Essa função adiciona um item no inicio do array
    array_unshift($marca_carros, 'Fiat');
    array_unshift($marca_carros, 'Hyundai');
+   array_unshift($marca_carros, 'Troller');
    
    var_dump($marca_carros);
 
