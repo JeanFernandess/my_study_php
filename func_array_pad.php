66e0bc224937d81e928c55ddf072901d89e3f3e8
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-17 22:58:39 -0300

    Update func_array_pad.php
    Adição da cor verde ao array $carros_cor[ ].

--- func_array_pad.php
+++ func_array_pad.php
@@ -6,7 +6,7 @@
      array_pad(Array,Numero de posicoes, valor das posicoes)
    */
        
-     $carros_cor = array('Amarelo', 'Vermelho', 'Azul');
+     $carros_cor = array('Amarelo', 'Vermelho', 'Azul', 'Verde');
      
      $carros_cor = array_pad($carros_cor, 5, 'Branco');
      
