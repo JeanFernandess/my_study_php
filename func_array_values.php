5a2fe9902050021013b0e053612d2279cca3c29c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-19 23:19:37 -0300

    Update func_array_values.php
    Adição do carro Troller ao array $carros[ ].

--- func_array_values.php
+++ func_array_values.php
@@ -12,7 +12,8 @@
                      'Car_3' => 'Gol',
                      'Car_4' => 'Ferrari',
                      'Car_5' => 'Chevette',
-                     'Car_6' => 'Jeep');
+                     'Car_6' => 'Jeep',
+                     'Car_7' => 'Troller');
      
      //array_values() so retorna o conteudo de cada indice
      $conteudo_array = array_values($carros);
