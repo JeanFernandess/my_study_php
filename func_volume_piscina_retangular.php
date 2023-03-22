d8442f3cc518a9b9b826174c3b653891cfffd12a
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-22 00:09:02 -0300

    Create func_volume_piscina_retangular.php
    Essa função mostra a quantidade de agua numa piscina retangular.

---
+++ func_volume_piscina_retangular.php
@@ -0,0 +1,15 @@
+<?php
+
+     /*Essa função recebe 3 argumentos e retorna o volume de agua
+       de uma piscina retangular.
+     
+     */
+   
+    function piscina_retangular($largura, $profundidade, $comprimento){
+    
+    $vol_agua = $largura * $comprimento * $profundidade;
+    echo 'A quantidade de agua na piscina é : ' . $vol_agua * 1000 . ' metros quadrados';
+    }
+    
+    piscina_retangular(5.0, 1.20, 15.0)
+?>
