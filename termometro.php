dd50502d931c21b099fe7c06ed1f830a0e7e275c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-03-30 17:12:50 -0300

    Create termometro.php
    Esse algoritmo lhe diz a condição da sua temperatura atual.

---
+++ termometro.php
@@ -0,0 +1,20 @@
+<?php
+      //Algoritmo para fins didáticos.
+     
+     $temperatura_corpo = 38.5;
+     
+     if($temperatura_corpo >= 36.1 && $temperatura_corpo <= 37.2){
+       
+       echo "Sua temperatura corporal esta ideal";
+     }else if($temperatura_corpo > 37.2){
+       
+       echo "Voce esta com febre!Cuide-se";
+     }else{
+       
+       echo "Voce esta com hipotermia!";
+     }
+     
+     
+     
+
+ ?>
