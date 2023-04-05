d40299fe524c69f6840b2d1f923a2638a6507bc1
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-05 00:00:36 -0300

    Create constant5.php
    Reforço e prática de constantes.

---
+++ constant5.php
@@ -0,0 +1,20 @@
+<?php
+      
+// Estudo e pratica de constantes.
+
+define("EMAIL", "aspirantedevelopment@gmail.com");
+define("SENHA", "12344321%$#@&&");
+
+
+
+if(EMAIL == "aspirantedevelopment@gmail.com" && SENHA == "12344321%$#@&&"){
+
+echo 'Email e senha ok';
+
+}else{
+
+echo 'Email ou senha invalido!';
+
+}
+
+?> 
