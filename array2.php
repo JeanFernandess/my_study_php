0aa02dd35d62c0e51265e8c9d8e51fbc35c9de40
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-05 00:23:44 -0300

    Create array2.php
    Estudo e pratica de Arrays.

---
+++ array2.php
@@ -0,0 +1,9 @@
+<?php
+   
+     //Estudo e praticas de array.
+       
+$cryptos = array("Bitcoin", "Xrp", "Dogecoin"); 
+
+echo "A crypto " . $cryptos[0] . " e a melhor entre todas as cryptos pois se mostrou forte e resistente ao longo do tempo. " . "A crypto " . $cryptos[1] . " depois que for encerrado o processo contra a SEC ela terpá um valor absurdo." . "Já a memecoin " . $cryptos[2] . " é uma crypto totalmente expeculativa, invista com consciencia que ela pode derreter a qualquer momento.";
+
+?>
