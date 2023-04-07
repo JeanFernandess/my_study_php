353ab4c3b4f16fe53a285b4b36ae6bd0e25ff160
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-06 22:13:11 -0300

    Update atribuicao_referencia.php
    Adicionando comentário na terceira linha do algoritmo.

--- atribuicao_referencia.php
+++ atribuicao_referencia.php
@@ -8,7 +8,7 @@
 
     $numero_1 = 1000;
     $numero_2 = &$numero_1; //$numero_2 = 1000
-    $numero_3 = &$numero_4;
+    $numero_3 = &$numero_4; //$numero_3 = 5
     $numero_4 = 5;
 
     $total = ($numero_1 + $numero_2)/$numero_3;
