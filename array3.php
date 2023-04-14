9c1d5fbbce634d57399327b08a71ad661d41884c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-14 17:16:22 -0300

    Create array3.php
    Estudo e prática de arrays e a estrutura de repetição foreach.

---
+++ array3.php
@@ -0,0 +1,17 @@
+<?php
+
+   //EXEMPLO SIMPLES DE ARRAYS E FOREACH.
+    
+   $sabor_cliente = 'Chocolate';
+
+   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti'];
+   
+   foreach($sabor_sorvete as $sabor){
+     if($sabor == $sabor_cliente){
+       echo 'O sabor escolhido é: ' .$sabor. "<br>";
+       echo 'O VALOR E : 2,00 REAIS';
+     }
+   } 
+     
+
+ ?>
