8c57f6a2504f52605d7c72d1681a1e8e47215bab
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-18 23:36:12 -0300

    Update array3.php
    Adição do sabor Doce de leite ao array sabor_sorvete.

--- array3.php
+++ array3.php
@@ -4,7 +4,7 @@
     
    $sabor_cliente = 'Chocolate';
 
-   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado'];
+   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado', 'Doce de Leite'];
    
    foreach($sabor_sorvete as $sabor){
      if($sabor == $sabor_cliente){
