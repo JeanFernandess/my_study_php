b0ae0da760cae9afc530550252f8b712c967f63c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-19 23:37:19 -0300

    Create if_else2.php
    Prática e fixação da estrutura de repetição if_else.

---
+++ if_else2.php
@@ -0,0 +1,19 @@
+<?php
+     
+     //Praticas com if else
+
+    $semana_trabalho = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado', 'Domingo'];
+    
+    foreach($semana_trabalho as $folga){
+      
+      if($folga == 'Sabado' || $folga == 'Domingo'){
+        echo 'O dia de ' .$folga. ' será a minha folga'. "<br>";
+      }else{
+      echo 'Hoje ' .$folga. ' e dia de trabalho!'. "<br>";
+      }
+      
+    }
+    
+     
+
+ ?>
