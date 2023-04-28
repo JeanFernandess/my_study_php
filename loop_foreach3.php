ebfdca4b68538da0696a275cd8fe540bf67beb49
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-28 17:29:32 -0300

    Create loop_foreach3.php
    Estudo do laço de repetição foreach.

---
+++ loop_foreach3.php
@@ -0,0 +1,12 @@
+?php
+
+     //Entendimento do bloco de repetição foreach
+     
+     $meu_humor = ['Triste', 'Pleno', 'Ansioso', 'Estressado', 'Raiva'];
+     
+     foreach($meu_humor as $humor){
+       echo 'Meu humor hoje esta : ' .$humor. "<br>";
+     }
+     
+
+ ?>
