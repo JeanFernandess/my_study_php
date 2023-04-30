fb8d67bf6f2220971a2a30545d87c832101931b8
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-29 22:34:21 -0300

    Update loop_foreach3.php
    Adição do item 'Depressão' ao array $meu_humor.

--- loop_foreach3.php
+++ loop_foreach3.php
@@ -2,7 +2,7 @@
 
      //Entendimento do bloco de repetição foreach
      
-     $meu_humor = ['Triste', 'Pleno', 'Ansioso', 'Estressado', 'Raiva'];
+     $meu_humor = ['Triste', 'Pleno', 'Ansioso', 'Estressado', 'Raiva', 'Depressivo'];
      
      foreach($meu_humor as $humor){
        echo 'Meu humor hoje esta : ' .$humor. "<br>";
