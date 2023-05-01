113f3fb1c1245a27f44e59e3a972e7772a010c47
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-04-30 23:13:37 -0300

    Update loop_foreach3.php
    Adição do item 'Melancólico ' ao array $meu_humor.

--- loop_foreach3.php
+++ loop_foreach3.php
@@ -2,7 +2,7 @@
 
      //Entendimento do bloco de repetição foreach
      
-     $meu_humor = ['Triste', 'Pleno', 'Ansioso', 'Estressado', 'Raiva', 'Depressivo'];
+     $meu_humor = ['Triste', 'Pleno', 'Ansioso', 'Estressado', 'Raiva', 'Depressivo', 'Melacolico'];
      
      foreach($meu_humor as $humor){
        echo 'Meu humor hoje esta : ' .$humor. "<br>";
