a368dab5aab686644fb26339c29b63b8337aa67b
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-05-10 00:02:48 -0300

    Update array3.php
    Adição do item Goiaba ao array sabor_sorvete().

--- array3.php
+++ array3.php
@@ -4,7 +4,7 @@
     
    $sabor_cliente = 'Chocolate';
 
-   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado', 'Doce de Leite', 'Abacate', 'Iogurte'];
+   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado', 'Doce de Leite', 'Abacate', 'Iogurte', 'Goiaba'];
    
    foreach($sabor_sorvete as $sabor){
      if($sabor == $sabor_cliente){
