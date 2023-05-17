1b0f59b3e3971b4013fd06d1b35acb220da5154c
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-05-16 22:28:58 -0300

    Update func_array_push.php
    Adição dos itens ao array $animais: boi, vaca e bezerro foram adicionados.

--- func_array_push.php
+++ func_array_push.php
@@ -5,6 +5,9 @@
    //Essa função adiciona um intem no final do array
    array_push($animais, 'Capivara');
    array_push($animais, 'Puma');
+   array_push($animais, 'Vaca');
+   array_push($animais, 'Boi');
+   array_push($animais, 'Bezerro');
 
    var_dump($animais);
 
