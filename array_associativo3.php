70face50ea4468bee8ba3c188646fd17d398aa97
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-05-18 22:20:38 -0300

    Update array_associativo3.php
    Adição da impressão da carteira02 e o seu saldo.Foi adicionado uma numeração aos índices para diferenciar dos outros arrays.

--- array_associativo3.php
+++ array_associativo3.php
@@ -2,22 +2,25 @@
 
     //Entendimento dos arrays associativos.
 
-     $carteira_bitcoin = array('CarteiraBitcoin' => 'Carteira01',
-                               'CarteiraSaldo' => '0.900000',
-                               'CarteiraSenha' => 'hhjsjsjsjsggfsjjsj',
-                               'CarteiraEndereco' => 'xhdgsjdugfsjjsgjey');
+     $carteira_bitcoin = array('CarteiraBitcoin01' => 'Carteira01',
+                               'CarteiraSaldo01' => '0.900000',
+                               'CarteiraSenha01' => 'hhjsjsjsjsggfsjjsj',
+                               'CarteiraEndereco01' => 'xhdgsjdugfsjjsgjey');
                                
                                
-    $carteira_etherium = array('CarteiraEtherium' => 'CarteiraEtherium02',
-                               'CarteiraSaldo' => '0.9865466',
-                               'CarteiraSenha' => '46gsh56gdvjs567',
-                               'CarteiraEndereco' => 'dhduevdgshsbv');
+    $carteira_etherium = array('CarteiraEtherium02' => 'CarteiraEtherium02',
+                               'CarteiraSaldo02' => '0.9865466',
+                               'CarteiraSenha02' => '46gsh56gdvjs567',
+                               'CarteiraEndereco02' => 'dhduevdgshsbv');
                                
-    $carteira_xrp = array('CarteiraXrp' => 'CarteiraXrp03',
-                          'CarteiraSaldo' => '20.000',
-                          'CarteiraSenha' => '52sjdb5gj57gkhf67bf',
-                          'CarteiraEndereco' => 'sgshsvdgshsjsgsj');
+    $carteira_xrp = array('CarteiraXrp03' => 'CarteiraXrp03',
+                          'CarteiraSaldo03' => '20.000',
+                          'CarteiraSenha03' => '52sjdb5gj57gkhf67bf',
+                          'CarteiraEndereco03' => 'sgshsvdgshsjsgsj');
                           
-    echo 'O nome da carteira e: ' . $carteira_bitcoin['CarteiraBitcoin']. "<br>";
-    echo 'O saldo da carteira é: '. $carteira_bitcoin['CarteiraSaldo'];
+    echo 'O nome da carteira 01 e: ' . $carteira_bitcoin['CarteiraBitcoin01']. "<br>";
+    echo 'O saldo da carteira 01 é: '. $carteira_bitcoin['CarteiraSaldo01']. "<br>";
+    echo "<br>";
+    echo 'O nome da carteira 02 e:' . $carteira_etherium['CarteiraEtherium02']. "<br>";
+    echo 'O saldo da carteira 02 e:' . $carteira_etherium['CarteiraSaldo02'] . "<br>";
  ?>
