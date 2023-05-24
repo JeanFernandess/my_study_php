a73fb77582a69a69da77c93f08a07cd0e18dd9d1
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-05-23 23:14:25 -0300

    Update array3.php
    Adição do sabor Graviola ao array $sabor_sorvete.

--- array3.php
+++ array3.php
@@ -4,7 +4,7 @@
     
    $sabor_cliente = 'Chocolate';
 
-   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado', 'Doce de Leite', 'Abacate', 'Iogurte', 'Goiaba', 'Maracuja'];
+   $sabor_sorvete = ['Chocolate', 'Morango', 'Flocos', 'Tutti-Frutti', 'Leite Condensado', 'Doce de Leite', 'Abacate', 'Iogurte', 'Goiaba', 'Maracuja', 'Graviola'];
    
    foreach($sabor_sorvete as $sabor){
      if($sabor == $sabor_cliente){
