4e69b561acccf4156e96b3a0cbee090d666ec7da
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-05-25 23:03:31 -0300

    Create object2.php
    Entendimento e prática de objetos na linguagem php

---
+++ object2.php
@@ -0,0 +1,21 @@
+<?php
+
+     
+
+     //Entendimento e pratica de objetos no php
+
+$backend_php = (object) array('Framework' => 'Laravel',
+
+                              'Linguagem' => 'Php',
+
+                              'Banco de dados' => 'Mysql',
+
+                              'Servidor' => 'Apache');
+
+                              
+
+ var_dump($backend_php);
+
+   
+
+ ?>
