df7fe7d0af45e0b3928b24e6e71716263b7b759a
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-06-07 22:05:24 -0300

    Create array4.php
    Prática e estudo de arrays no php.

---
+++ array4.php
@@ -0,0 +1,17 @@
+<?php
+
+   //Pratica de arrays no php
+
+$times = array('Flamengo', 'Gremio', 'Sao paulo', 'Fortaleza');
+
+ foreach($times as $time){
+  if($time == 'Flamengo'){
+     echo "O time: $time é o melhor do mundo <br>";
+  }
+  echo "O time: $time é meia boca. <br>";
+ 
+ }
+ 
+ 
+ 
+?> 
