cb2a9032a51ae108e8237e539b9df71629374bd5
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-06-07 22:47:41 -0300

    Create constante7.php
    Estudo e pratica de constantes.

---
+++ constante7.php
@@ -0,0 +1,24 @@
+<?php
+     
+     
+     //Pratica de constantes
+
+
+   define("NAMORADA1", "Julia");
+   define("NAMORADA2", "Camila");
+   define("NAMORADA3", "Andrea");
+   
+   
+   if(NAMORADA1 == "Julia"){
+      echo "A namorada ".NAMORADA1. "foi a namorada mais compreesiva de todas";
+      
+   }elseif(NAMORADA2 == "Camila"){
+      echo "A namorada " .NAMORADA2. " foi aonde tudo comecou.";
+      
+   }else{
+      echo "A namorada " .NAMORADA3. " que Deus abençõe";
+   }
+    
+   
+ 
+?> 
